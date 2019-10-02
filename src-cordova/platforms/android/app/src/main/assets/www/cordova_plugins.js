24699f1d57c7a9a9da1b40d7950e765f40d30ba4
Luis Alfonso Rojo Sánchez <luis_alfonso_96@hotmail.com>
2019-10-02 10:24:44 -0500

    Ahora los proyectos de las plataformas se suben al repositorio

---
+++ src-cordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open",
+        "window.open"
+      ]
+    },
+    {
+      "id": "phonegap-plugin-push.PushNotification",
+      "file": "plugins/phonegap-plugin-push/www/push.js",
+      "pluginId": "phonegap-plugin-push",
+      "clobbers": [
+        "PushNotification"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-inappbrowser": "3.1.0",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-support-google-services": "1.3.1",
+    "phonegap-plugin-multidex": "1.0.0",
+    "phonegap-plugin-push": "2.3.0"
+  };
+});
